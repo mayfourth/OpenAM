74002f7c9316664c841e5e5f3a0a46f1f128a972
rwapshott <rwapshott@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2014-02-17 14:47:38 +0000

    Merging 8003 8004 8005 8008 8009 8010 8011 8012 8013 8029 8037 8040 8041 8042 8043 8051
    git-svn-id: https://svn.forgerock.org/openam/branches/OPENAM-3442-CTS-TokenType@8100 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- opensso/products/webagents/agents/source/apache24/apache_agent.c
+++ opensso/products/webagents/agents/source/apache24/apache_agent.c
@@ -26,7 +26,7 @@
  */
 
 /*
- * Portions Copyrighted 2011-2013 ForgeRock Inc
+ * Portions Copyrighted 2011-2014 ForgeRock AS
  */
 
 #include <limits.h>
@@ -925,6 +925,9 @@ static am_status_t render_result(void **args, am_web_result_t http_result, char 
             case AM_WEB_RESULT_ERROR:
                 *apache_ret = HTTP_INTERNAL_SERVER_ERROR;
                 break;
+            case AM_WEB_RESULT_NOT_IMPLEMENTED:
+                *apache_ret = HTTP_NOT_IMPLEMENTED ;
+                break;
             default:
                 am_web_log_error("%s: Unrecognized process result %d.", thisfunc, http_result);
                 *apache_ret = HTTP_INTERNAL_SERVER_ERROR;
@@ -947,6 +950,7 @@ static am_status_t get_request_url(request_rec *r, char **requestURL) {
         status = AM_FAILURE;
     } else {
         char *url = *requestURL;
+#ifdef OPENAM_2969
         am_web_log_debug("%s: request url before normalization: %s", thisfunc, url);
         /*find the end of url string*/
         while (url && *url) ++url;
@@ -956,6 +960,7 @@ static am_status_t get_request_url(request_rec *r, char **requestURL) {
                 *url = 0;
             } else break;
         }
+#endif
     }
     am_web_log_debug("%s: returning request url: %s", thisfunc, *requestURL);
     return status;
@@ -1400,6 +1405,7 @@ int agent_check_access(request_rec *r) {
         req_params.path_info = r->path_info;
         req_params.cookie_header_val =
                 (char *) apr_table_get(r->headers_in, "Cookie");
+        req_params.content_type = (char *) apr_table_get(r->headers_in, "Content-Type");
         req_func.get_post_data.func = content_read;
         req_func.get_post_data.args = args;
         // no free_post_data
