b2c09fe8c561043dad3d6804681e47d5f0ec1ac1
dhogan <dhogan@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2014-07-17 23:15:58 +0000

    ongoing work wiring the ViewBean save button into the rest-sts publish service.
    git-svn-id: https://svn.forgerock.org/openam/branches/rest_sts_view_bean@9767 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- openam-console/src/main/java/com/sun/identity/console/service/RestSecurityTokenServiceViewBean.java
+++ openam-console/src/main/java/com/sun/identity/console/service/RestSecurityTokenServiceViewBean.java
@@ -9,10 +9,44 @@ import com.sun.identity.console.base.model.AMConsoleException;
 import com.sun.identity.console.base.model.AMPropertySheetModel;
 import com.sun.identity.console.base.model.AMServiceProfileModel;
 import com.sun.web.ui.view.alert.CCAlert;
+import org.forgerock.json.fluent.JsonValue;
+import org.restlet.data.MediaType;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import static org.forgerock.json.fluent.JsonValue.field;
+import static org.forgerock.json.fluent.JsonValue.json;
+import static org.forgerock.json.fluent.JsonValue.object;
 
 public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBase {
+    /*
+    This field matches that defined in the AMSTSConstants. I cannot introduce a dependency on the rest-sts in the
+    openam-console module, so this value must be duplicate here.
+     */
+    private static final String REST_STS_PUBLISH_INVOCATION_CONTEXT = "invocation_context";
+
+    /*
+    This field matches that defined in the AMSTSConstants. I cannot introduce a dependency on the rest-sts in the
+    openam-console module, so this value must be duplicate here.
+     */
+    private static final String REST_STS_PUBLISH_INVOCATION_CONTEXT_VIEW_BEAN = "invocation_context_view_bean";
+
+    /*
+    This field matches that defined in the AMSTSConstants. I cannot introduce a dependency on the rest-sts in the
+    openam-console module, so this value must be duplicate here.
+     */
+    public static final String REST_STS_PUBLISH_INSTANCE_STATE = "instance_state";
+
+
     public static final String DEFAULT_DISPLAY_URL =
             "/console/service/RestSecurityTokenService.jsp";
 
@@ -62,8 +96,26 @@ public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBa
         This is the code directly from the superclass. Presumably it saves state in the sms ? I need to
         marshal a RestSTSInstanceConfig instance from the attributes and call the
         rest sts publish service here...
+        Also I need to set the amDeploymentUrl TODO
          */
         submitCycle = true;
+        Map<String, Set<String>> configurationState = (Map<String, Set<String>>)getAttributeValues();
+        JsonValue propertiesMap = new JsonValue(marshalSetValuesToListValues(configurationState));
+        JsonValue invocationJson = json(object(
+                field(REST_STS_PUBLISH_INVOCATION_CONTEXT, REST_STS_PUBLISH_INVOCATION_CONTEXT_VIEW_BEAN),
+                field(REST_STS_PUBLISH_INSTANCE_STATE, propertiesMap)));
+        try {
+            Representation representation = new ClientResource(getRestSTSPublishEndpointUrl())
+                    .post(new StringRepresentation(invocationJson.toString(), MediaType.APPLICATION_JSON));
+            setInlineAlertMessage(CCAlert.TYPE_INFO, "message.information",
+                    representation.getText()); //TODO - create an I18N message that just displays success and url fragment
+        } catch (ResourceException re) {
+            //the Restlet ResourceException only displays status in toString method.
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error", re.getMessage());
+        } catch (Exception e) {
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error", e.toString());
+        }
+/*
         AMServiceProfileModel model = (AMServiceProfileModel)getModel();
 
         if (model != null) {
@@ -71,6 +123,7 @@ public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBa
                 Map values = getValues();
                 onBeforeSaveProfile(values);
                 model.setAttributeValues(values);
+
                 setInlineAlertMessage(CCAlert.TYPE_INFO, "message.information",
                         "message.updated");
             } catch (AMConsoleException e) {
@@ -78,7 +131,28 @@ public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBa
                         e.getMessage());
             }
         }
+ */
         forwardTo();
+    }
+
+    private String getRestSTSPublishEndpointUrl() {
+        //TODO: proper endpoint
+        return "http://localhost:8080/openam/rest-sts-publish/publish?_action=create";
+    }
+
+    /*
+    Currently, JsonValue#toString will only create a json array for elements which are lists. If I want the
+    Map<String, Set<String>> returned by this.getValues() to marshal to json correctly using JsonValue#toString(), I
+    need to transform the Map<String, Set<String>> to a Map<String, List<String>>.
+     */
+    private Map<String, List<String>> marshalSetValuesToListValues(Map<String, Set<String>> smsMap) {
+        Map<String, List<String>> listMap = new HashMap<String, List<String>>();
+        for (Map.Entry<String, Set<String>> entry : smsMap.entrySet()) {
+            ArrayList<String> list = new ArrayList<String>(entry.getValue().size());
+            list.addAll(entry.getValue());
+            listMap.put(entry.getKey(), list);
+        }
+        return listMap;
     }
 
     /**
