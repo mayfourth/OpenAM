1fb5ab15bc6b001927bb4038a157c9f6d922560d
dhogan <dhogan@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2014-07-21 18:07:20 +0000

    now using HttpURLConnection instead of Restlet ClientResource. Updated error and success displays, and some input configuration state validation.
    git-svn-id: https://svn.forgerock.org/openam/branches/rest_sts_view_bean@9814 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- openam-console/src/main/java/com/sun/identity/console/service/RestSecurityTokenServiceViewBean.java
+++ openam-console/src/main/java/com/sun/identity/console/service/RestSecurityTokenServiceViewBean.java
@@ -2,6 +2,8 @@ package com.sun.identity.console.service;
 
 import com.iplanet.jato.model.ModelControlException;
 import com.iplanet.jato.view.event.RequestInvocationEvent;
+import com.iplanet.sso.SSOToken;
+import com.sun.identity.common.HttpURLConnectionManager;
 import com.sun.identity.console.base.AMPropertySheet;
 import com.sun.identity.console.base.AMServiceProfileViewBeanBase;
 import com.sun.identity.console.base.AMViewBeanBase;
@@ -12,15 +14,22 @@ import com.sun.identity.console.base.model.AMConsoleException;
 import com.sun.identity.console.base.model.AMPropertySheetModel;
 import com.sun.identity.console.base.model.AMServiceProfileModel;
 import com.sun.identity.console.base.model.AMSystemConfig;
+import com.sun.identity.security.AdminTokenAction;
+import com.sun.identity.shared.debug.Debug;
+import com.sun.identity.sm.OrganizationConfigManager;
+import com.sun.identity.sm.SMSException;
 import com.sun.web.ui.view.alert.CCAlert;
 import org.forgerock.json.fluent.JsonValue;
-import org.restlet.data.MediaType;
-import org.restlet.representation.Representation;
-import org.restlet.representation.StringRepresentation;
-import org.restlet.resource.ClientResource;
-import org.restlet.resource.ResourceException;
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.AccessController;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -33,6 +42,12 @@ import static org.forgerock.json.fluent.JsonValue.json;
 import static org.forgerock.json.fluent.JsonValue.object;
 
 public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBase {
+    private static final Debug debug = Debug.getInstance("sts");
+    /*
+    Review TODO: perhaps the values below which match those defined in AMSTSConstants should be moved to a module
+    shared between openam-sts and openam-console? Perhaps openam-shared?? Or should the values just be duplicated??
+     */
+
     /*
     This field matches that defined in the AMSTSConstants. I cannot introduce a dependency on the rest-sts in the
     openam-console module, so this value must be duplicate here.
@@ -49,13 +64,12 @@ public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBa
     This field matches that defined in the AMSTSConstants. I cannot introduce a dependency on the rest-sts in the
     openam-console module, so this value must be duplicate here.
      */
-    public static final String REST_STS_PUBLISH_INSTANCE_STATE = "instance_state";
+    private static final String REST_STS_PUBLISH_INSTANCE_STATE = "instance_state";
 
     /*
     This String must match the string defined in STSInstanceConfig.AM_DEPLOYMENT_URL.
      */
-    public static final String AM_DEPLOYMENT_URL = "am-deployment-url";
-
+    private static final String AM_DEPLOYMENT_URL = "am-deployment-url";
 
     public static final String DEFAULT_DISPLAY_URL =
             "/console/service/RestSecurityTokenService.jsp";
@@ -103,54 +117,21 @@ public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBa
     public void handleButton1Request(RequestInvocationEvent event) throws ModelControlException {
         submitCycle = true;
         Map<String, Set<String>> configurationState = (Map<String, Set<String>>)getAttributeSettings();
-        /*
-        Add the url corresponding to the am deployment, as this information does not have to be solicited from the user.
-         */
-        Set<String> deploymentUrlSet = new HashSet<String>();
-        deploymentUrlSet.add(getAMDeploymentUrl());
-        configurationState.put(AM_DEPLOYMENT_URL, deploymentUrlSet);
-        JsonValue propertiesMap = new JsonValue(marshalSetValuesToListValues(configurationState));
-        JsonValue invocationJson = json(object(
-                field(REST_STS_PUBLISH_INVOCATION_CONTEXT, REST_STS_PUBLISH_INVOCATION_CONTEXT_VIEW_BEAN),
-                field(REST_STS_PUBLISH_INSTANCE_STATE, propertiesMap)));
-        Representation representation;
-        try {
-            representation = new ClientResource(getRestSTSPublishEndpointUrl())
-                    .post(new StringRepresentation(invocationJson.toString(), MediaType.APPLICATION_JSON));
-            setInlineAlertMessage(CCAlert.TYPE_INFO, "message.information",
-                    representation.getText()); //TODO - create an I18N message that just displays success and url fragment
-        } catch (ResourceException re) {
-
-            //the Restlet ResourceException only displays status in toString method.
-            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error", re.getMessage());
-        } catch (Exception e) {
-            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error", e.toString());
+        addOpenAMDeploymentUrlToConfigurationState(configurationState);
+        if (configurationStateValid(configurationState)) {
+            JsonValue propertiesMap = new JsonValue(marshalSetValuesToListValues(configurationState));
+            JsonValue invocationJson = json(object(
+                    field(REST_STS_PUBLISH_INVOCATION_CONTEXT, REST_STS_PUBLISH_INVOCATION_CONTEXT_VIEW_BEAN),
+                    field(REST_STS_PUBLISH_INSTANCE_STATE, propertiesMap)));
+            invokeRestSTSPublishService(invocationJson.toString());
         }
-/*
-        AMServiceProfileModel model = (AMServiceProfileModel)getModel();
-
-        if (model != null) {
-            try {
-                Map values = getValues();
-                onBeforeSaveProfile(values);
-                model.setAttributeValues(values);
-
-                setInlineAlertMessage(CCAlert.TYPE_INFO, "message.information",
-                        "message.updated");
-            } catch (AMConsoleException e) {
-                setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
-                        e.getMessage());
-            }
-        }
- */
         forwardTo();
     }
 
     /*
     Returns a map of all settings, including those not changed from the default values in the model.
      */
-    private Map getAttributeSettings()
-            throws ModelControlException {
+    private Map getAttributeSettings() throws ModelControlException {
         Map values = null;
         AMServiceProfileModel model = (AMServiceProfileModel)getModel();
 
@@ -162,8 +143,141 @@ public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBa
                 throw new ModelControlException(e.getMessage(), e);
             }
         }
-
         return values;
+    }
+
+    /*
+    Add the url corresponding to the am deployment, as this information does not have to be solicited from the user.
+     */
+    private void addOpenAMDeploymentUrlToConfigurationState(Map<String, Set<String>> configurationState) {
+        Set<String> deploymentUrlSet = new HashSet<String>();
+        deploymentUrlSet.add(getAMDeploymentUrl());
+        configurationState.put(AM_DEPLOYMENT_URL, deploymentUrlSet);
+    }
+
+    /*
+    performs validation checks on user-configured state.
+     */
+    private boolean configurationStateValid(Map<String, Set<String>> configurationState) {
+        if (isNullOrEmpty(configurationState.get("saml2-token-lifetime-seconds"))) {
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                    "SAML2 Token lifetime must be specified.");
+            return false;
+        }
+        if (isNullOrEmpty(configurationState.get("deployment-realm"))) {
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                    "Deployment realm must be specified.");
+            return false;
+        } else {
+            String realm = configurationState.get("deployment-realm").iterator().next();
+            if (!isValidRealm(realm)) {
+                setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                        "Specified deployment realm is not a valid realm.");
+                return false;
+            }
+        }
+        if (isNullOrEmpty(configurationState.get("deployment-url-element"))) {
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                    "Deyployment Url element must be specified.");
+            return false;
+        } else {
+            String urlElement = configurationState.get("deployment-url-element").iterator().next();
+            if (urlElement.contains("/")) {
+                setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                        "Deployment Url element can neither start, end, nor contain, the '/' character.");
+                return false;
+            }
+        }
+
+        if (isNullOrEmpty(configurationState.get("issuer-name"))) {
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                    "Issuer name must be specified.");
+            return false;
+        }
+        //keystore state - perhaps open it?? at least has to be specified
+        return true;
+    }
+
+    private void invokeRestSTSPublishService(String invocationPayload) {
+        try {
+            URL url = new URL(getRestSTSPublishEndpointUrl());
+            HttpURLConnection connection = HttpURLConnectionManager.getConnection(url);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(invocationPayload);
+            writer.close();
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_CREATED) {
+                displaySuccessMessage(connection);
+            } else {
+                displayErrorMessage(connection);
+            }
+        } catch (MalformedURLException e) {
+            debug.error("Exception creating rest-sts publish service url in " +
+                    "RestSecurityTokenServiceViewBean#invokeRestSTSPublishService: " + e, e);
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                    "Exception creating rest-sts publish service url: " + e.getMessage());
+        } catch (IOException e) {
+            debug.error("Exception invoking rest sts publish service in " +
+                    "RestSecurityTokenServiceViewBean#invokeRestSTSPublishService: " + e, e);
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                    "Exception invoking rest sts publish service: " + e.getMessage());
+        }
+    }
+
+    private void displaySuccessMessage(HttpURLConnection connection) {
+        try {
+            //TODO - I18N?
+            final String successMessage = readInputStream(connection.getInputStream());
+            setInlineAlertMessage(CCAlert.TYPE_INFO, "message.information", successMessage);
+        } catch (IOException e) {
+            debug.error("Exception in RestSecurityTokenServiceViewBean#displaySuccessMessage: " + e, e);
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                    "Exception getting response from rest sts publish service : " + e.getMessage());
+        }
+    }
+
+    private void displayErrorMessage(HttpURLConnection connection) {
+        //TODO: not sure I should get content of input or error stream
+        try {
+            String errorMessage = null;
+            if (connection.getErrorStream() != null) {
+                errorMessage = readInputStream(connection.getErrorStream());
+            } else {
+                errorMessage = readInputStream(connection.getInputStream());
+            }
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                    "Exception getting response from rest sts publish service: " + errorMessage);
+        } catch (IOException e) {
+            debug.error("Exception in RestSecurityTokenServiceViewBean#displayErrorMessage: " + e, e);
+            setInlineAlertMessage(CCAlert.TYPE_ERROR, "message.error",
+                    "Exception getting response from rest sts publish service: " + e.getMessage());
+        }
+    }
+
+    private String readInputStream(InputStream inputStream) throws IOException {
+        if (inputStream == null) {
+            return "Empty error stream";
+        } else {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder stringBuilder = new StringBuilder();
+            try {
+                String str;
+                while ((str = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(str);
+                }
+                return stringBuilder.toString();
+            } finally {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ioe) {
+                    debug.warning("Exception closing BufferedReader in finally block in RestSecurityTokenViewBean#readInputStream: "
+                            + ioe.getMessage(), ioe);
+                }
+            }
+        }
     }
 
     private String getRestSTSPublishEndpointUrl() {
@@ -194,32 +308,6 @@ public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBa
      *
      * @param event Request invocation event
      *
-    public void handleButton3Request(RequestInvocationEvent event) {
-    --no worky--
-    removePageSessionAttribute(PAGE_MODIFIED);
-    backTrail();
-    try {
-    String name = (String) getPageSessionAttribute(
-    AMAdminConstants.SAVE_VB_NAME);
-    SCConfigViewBean vb = (SCConfigViewBean) getViewBean(
-    Class.forName(name));
-    passPgSessionMap(vb);
-    vb.forwardTo(getRequestContext());
-    } catch (ClassNotFoundException e) {
-    debug.warning(
-    "RestSecurityTokenServiceViewBean.handleButton3Request:", e);
-    }
-
-    --possible??--
-    MAPClientManagerViewBean vb = (MAPClientManagerViewBean)getViewBean(
-    MAPClientManagerViewBean.class);
-    passPgSessionMap(vb);
-    vb.forwardTo(getRequestContext());
-
-    --this did nothing--
-    super.handleButton2Request(event);
-
-    }
      */
     public void handleButton3Request(RequestInvocationEvent event)
             throws ModelControlException, AMConsoleException {
@@ -236,7 +324,7 @@ public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBa
         PageTrail.Marker marker = backTrail();
         if (marker != null) {
             /*
-            This branch seems never to be entered. Perhaps remove? TODO:
+            This branch seems never to be entered as backTrail always seems to return null. Perhaps remove? TODO:
              */
             try {
                 return (AMViewBeanBase) getViewBean(Class.forName(marker.getViewBeanClassName()));
@@ -271,4 +359,28 @@ public class RestSecurityTokenServiceViewBean extends AMServiceProfileViewBeanBa
             }
         }
     }
+
+    private boolean isNullOrEmpty(Set<String> set) {
+        return ((set == null) || set.isEmpty());
+    }
+
+    public boolean isValidRealm(String token) {
+        try {
+            new OrganizationConfigManager(getSSOToken(), token);
+        } catch (SMSException e) {
+            // Cannot find realm
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Gets the Admin SSO Token.
+     *
+     * @return The Admin SSO Token.
+     */
+    private SSOToken getSSOToken() {
+        return AccessController.doPrivileged(AdminTokenAction.getInstance());
+    }
+
 }
