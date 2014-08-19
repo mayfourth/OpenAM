9f2620387199b1006d946344001b374b93f532a7
jamesphillpotts <jamesphillpotts@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2014-08-19 13:18:28 +0000

    AME-3423 CR-4257 Remove unused code
    git-svn-id: https://svn.forgerock.org/openam/branches/AME-3423@10180 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- openam/openam-core/src/main/java/com/iplanet/dpro/session/operations/ServerSessionOperationStrategy.java
+++ openam/openam-core/src/main/java/com/iplanet/dpro/session/operations/ServerSessionOperationStrategy.java
@@ -109,48 +109,8 @@ public class ServerSessionOperationStrategy implements SessionOperationStrategy 
         if (service.isCrossTalkEnabled() || !cts.hasSession(session)) {
             return logAndWrap(session, remote, SessionMonitorType.REMOTE);
         }
-//        if (service.isSessionFailoverEnabled()) {
-//            // Cross talk is disabled.
-//            if (!service.isCrossTalkEnabled()) {
-//                return logAndWrap(session, cts);
-//            }
-//
-//            // Remote Site which is known to be down
-//            if (!isLocalSite(session) && !isSiteUp(getSiteId(session))) {
-//                return logAndWrap(session, cts);
-//            }
-//        }
 
         return logAndWrap(session, cts, SessionMonitorType.CTS);
-    }
-
-    /**
-     * Fetches the Site for a Session Server ID, based on the results of WebtopNaming.
-     * @param session A non null Session which may or may not be part of a Site.
-     * @return Null if no Site ID was found, otherwise a non null Site ID.
-     */
-    private String getSiteId(Session session) {
-        String serverID = session.getID().getSessionServerID();
-        if (queryUtils.isSite(serverID)) {
-            return serverID;
-        }
-        return queryUtils.getSiteID(serverID);
-    }
-
-    /**
-     * Indicates that the Site associated with the Session is up.
-     *
-     * @param siteId Site ID to test if it is up. May be null, in which case
-     *               false will be returned.
-     * @return False if the Site ID is null, or if the Site was down.
-     * True if the Site was up.
-     */
-    private boolean isSiteUp(String siteId) {
-        if (siteId == null) {
-            return false;
-        }
-
-        return service.checkSiteUp(siteId);
     }
 
     /**
@@ -168,21 +128,10 @@ public class ServerSessionOperationStrategy implements SessionOperationStrategy 
     }
 
     /**
-     * Indicates that the Session belongs to a the local Site. That is, it is
-     * hosted on a Server within the current cluster.
-     *
-     * @param session Non null Session.
-     * @return True if the Session is considered local.
-     */
-    private boolean isLocalSite(Session session) {
-        return service.isLocalSite(session.getID());
-    }
-
-    /**
      * Inline logging function.
      * @param session Non null.
      * @param op Non null operation selected.
-     * @param local
+     * @param type
      * @return op.
      */
     private SessionOperations logAndWrap(Session session, SessionOperations op, SessionMonitorType type) {
